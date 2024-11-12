33481ad94807d248f3a7befee75e593379e6fa72
HowardHung <howard1980041@gmail.com>
2024-11-12 16:58:13 +0800

    09

--- js/controller.js
+++ js/controller.js
@@ -1,29 +1,29 @@
 import * as model from './model.js';
 import TodoView from './views/TodoView.js';
-import Finishitem from './views/Finishitem.js';
+import FinishItem from './views/FinishItem.js';
 
 const controlAddNote = function (text) {
   model.addNote(text);
   TodoView.render(model.state.notes);
-  Finishitem.render(model.state.notes);
+  FinishItem.render(model.state.notes);
 };
 
 const controlAddTitle = function (newNote) {
   model.addNote(newNote);
   TodoView.render(model.state.notes);
-  Finishitem.render(model.state.notes);
+  FinishItem.render(model.state.notes);
 };
 
 const controlDeleteNote = function (id) {
   model.deleteNote(id);
   TodoView.render(model.state.notes);
-  Finishitem.render(model.state.notes);
+  FinishItem.render(model.state.notes);
 };
 
 const controlEditNote = function (id, newText) {
   model.editNote(id, newText);
   TodoView.render(model.state.notes);
-  Finishitem.render(model.state.notes);
+  FinishItem.render(model.state.notes);
 };
 
 const controlReorder = function (fromIndex, toIndex) {
@@ -33,13 +33,13 @@ const controlReorder = function (fromIndex, toIndex) {
 const controlToggleCheck = function (id) {
   model.toggleCheck(id);
   TodoView.render(model.state.notes);
-  Finishitem.render(model.state.notes);
+  FinishItem.render(model.state.notes);
 };
 
 const init = function () {
   TodoView.render(model.state.notes);
-  Finishitem.render(model.state.notes);
-  Finishitem.addHandlerReset(controlToggleCheck);
+  FinishItem.render(model.state.notes);
+  FinishItem.addHandlerReset(controlToggleCheck);
   TodoView.addHandlerAddItem(controlAddNote);
   TodoView.addHandlerDelete(controlDeleteNote);
   TodoView.addHandlerEdit(controlEditNote);
